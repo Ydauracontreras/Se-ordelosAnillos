75507e047f23b65bb7c23fee49fc493f412aa54c
lauraizco24 <lauraizquierdo24@gmail.com>
2020-04-12 08:59:56 -0300

    Se Agregan las clases que faltaban.

---
+++ LOTR/src/app/personajes/Goblin.java
@@ -0,0 +1,31 @@
+package app.personajes;
+
+import app.reliquias.Reliquia;
+
+public class Goblin extends Criatura {
+
+    public int energiaMagica;
+    public Reliquia reliquia;
+
+    public Goblin(String nombre, int salud, int stamina) {
+        super(nombre, salud, stamina);
+        // TODO Auto-generated constructor stub
+    }
+
+
+    public int getEnergiaMagica() {
+        return energiaMagica;
+    }
+
+    public Reliquia getReliquia() {
+        return reliquia;
+    }
+
+    public void setEnergiaMagica(int energiaMagica) {
+        this.energiaMagica = energiaMagica;
+    }
+
+    public void setReliquia(Reliquia reliquia) {
+        this.reliquia = reliquia;
+    }
+}
---
+++ LOTR/src/app/reliquias/FrascoGaladriel.java
@@ -0,0 +1,5 @@
+package app.reliquias;
+
+public class FrascoGaladriel extends Reliquia{
+
+}
